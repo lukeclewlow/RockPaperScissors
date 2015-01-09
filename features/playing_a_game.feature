66b31fc84eacde7582054b7ea767f49db9034958
Luke Clewlow <luke.clewlow@gmail.com>
2015-01-09 12:29:06 +0000

    created file structure and first tests

---
+++ features/playing_a_game.feature
@@ -0,0 +1,11 @@
+Feature: Playing a game
+	As a bored individual
+	In order to play a game of RPS
+	I want to pick an option and see if I win
+
+	Scenario:
+		Given I am on the homepage
+		Then I enter my name
+		Then I will see a welcome greeting
+
+	
---
+++ features/step_definitions/playing_a_game_steps.rb
@@ -0,0 +1,13 @@
+require 'sinatra/base'
+
+Given(/^I am on the homepage$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I enter my name$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I will see a welcome greeting$/) do
+  pending # express the regexp above with the code you wish you had
+end
